7e78af64b92eb64fecb73d71b93eeac22921d003
ntsakonas <magicnickus@yahoo.com>
2021-01-29 10:19:17 +0000

    added skeleton project

---
+++ src/main/java/com/ntsakonas/picourl/PicourlApplication.java
@@ -0,0 +1,13 @@
+package com.ntsakonas.picourl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicourlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicourlApplication.class, args);
+	}
+
+}
