e8de604432dea8ac906e01204b9c8b96308e9f55
ntsakonas <magicnickus@yahoo.com>
2021-01-29 16:43:52 +0000

    using an in-mem repository until db is in place

--- src/main/java/com/ntsakonas/picourl/ShortenedUrlRepository.java
+++ src/main/java/com/ntsakonas/picourl/ShortenedUrlRepository.java
@@ -1,17 +1,93 @@
 package com.ntsakonas.picourl;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class ShortenedUrlRepository {
+
+    private static class RepositoryStats {
+
+        int numOfLookups = 0;
+        int numOfHits = 0;
+        int numOfMisses = 0;
+        int numOfWrites = 0;
+
+        void lookup() {
+            numOfLookups++;
+        }
+
+        void write() {
+            numOfWrites++;
+        }
+
+        void miss() {
+            numOfMisses++;
+        }
+
+        void hit() {
+            numOfHits++;
+        }
+
+        @Override
+        public String toString() {
+            return "RepositoryStats: " +
+                    " numOfLookups=" + numOfLookups +
+                    " numOfHits=" + numOfHits +
+                    " numOfMisses=" + numOfMisses +
+                    " numOfWrites=" + numOfWrites;
+        }
+    }
+
+    // shortUrl->longUrl
+    private final Map<String, String> imMemCache = new HashMap<>();
+    // longUrl->shortUrl
+    private final Map<String, String> imMemReverseCache = new HashMap<>();
+
+    private final RepositoryStats stats = new RepositoryStats();
+
 
     public Set<Long> getHashValues(List<Long> shortHashValues) {
         Set<Long> usedValues = new HashSet<>() {{
             // add(7835846572003L);
             // add(467513369371L);
         }};
-
+        stats.lookup();
         return usedValues;
+    }
+
+    public void saveShortenedUrl(String longUrl, String shortUrl, long hashValue) {
+        imMemCache.put(shortUrl, longUrl);
+        imMemReverseCache.put(longUrl, shortUrl);
+        stats.write();
+    }
+
+    // resolve a short url back to the original long url
+    public Optional<String> getLongUrl(String shortUrl) {
+
+        Optional<String> longUrl = Optional.ofNullable(imMemCache.get(shortUrl));
+
+        if (longUrl.isPresent()) stats.hit();
+        else stats.miss();
+
+        return longUrl;
+    }
+
+    // get the short url associated with a long url
+    // used for pre-checking, if the long url is already shortened
+    // avoid doing it again. The conflict in the hashing will force the long url
+    // to be mapped to a different short url, wasting space
+    // at the expense of having another db read we can save space.
+    public Optional<String> getShortUrl(String longUrl) {
+        Optional<String> shortUrl = Optional.ofNullable(imMemReverseCache.get(longUrl));
+
+        if (shortUrl.isPresent()) stats.hit();
+        else stats.miss();
+
+        return shortUrl;
+    }
+
+    public void showStats() {
+        System.out.println("------------------");
+        System.out.println(stats);
+        System.out.println("------------------");
     }
 }
