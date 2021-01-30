c81093a64d2b13dfb5f2f124a63d0f28de9ecef5
ntsakonas <magicnickus@yahoo.com>
2021-01-30 02:31:10 +0000

    increased the maximum length of a long url to 150 characters

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,6 @@ CREATE TABLE shortened_urls (
   id BIGINT AUTO_INCREMENT  PRIMARY KEY,
   hash_value BIGINT NOT NULL,
   short_url VARCHAR(10) NOT NULL,
-  long_url VARCHAR(100) NOT NULL,
-  created_at INT NOT NULL
+  long_url VARCHAR(150) NOT NULL,
+  created_at BIGINT NOT NULL
 );
